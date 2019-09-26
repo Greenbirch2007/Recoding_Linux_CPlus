48bf5b57f5f9d272ab97f60c54d9fa770443b9ba
Greenbirch2007 <291109028@qq.com>
2019-09-26 23:05:01 +0800

    函数调用

--- TC/t6.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-
-int main()
-{
-	int a=1,b=2;
-	cout<< !a<<"------------"<<!(a-b) <<endl;
-	cout<< a&&b <<"-------------"<<a&&(a-1) <<endl;
-	cout<<a||b << "----------"<<a||(b-2) <<endl;
-	return 0;}
---
+++ tc/c1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<iomanip>
+
+using namespace std;
+
+int main()
+{
+	float x=1233.79464;
+	cout<< x<< endl;
+	cout.precision(5);
+	cout.width(10);
+	cout<<x<<endl;
+	return 0;
+}
+
+
+
